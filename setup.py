883f1bf7cf0850e5c58661ecee1231961517b357
Brian Thorne <brian@thorne.link>
2017-11-28 21:18:39 +1100

    More updates to get ready for packaging

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
 
 requirements = [
         "bitarray==0.8.1",
         "networkx==1.11",
-        "cffi>=1.4.1",
+        "cffi>=1.7",
     ]
 
 setup(
     name="anonlink",
-    version='0.6.0',
+    author='Brian Thorne',
+    author_email='brian.thorne@data61.csiro.au',
+    version='0.6.1-rc.3',
     description='Anonymous linkage using cryptographic hashes and bloom filters',
-    url='https://github.inside.nicta.com.au/magic/AnonymousLinking',
+    long_description=open(os.path.join(here, "README.rst")).read(),
+    url='https://github.com/n1analytics/anonlink',
     license='Apache',
-    setup_requires=requirements,
+    setup_requires=['cffi>=1.7'],
     install_requires=requirements,
-    test_requires=requirements,
-    packages=find_packages(exclude=['cpp_code', 'cpp_code.*', 'tests']),
+    packages=find_packages(exclude=[
+        '_cffi_build', '_cffi_build/*',
+        'tests'
+    ]),
     package_data={'anonlink': ['data/*.csv']},
+
+    ext_package="anonlink",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Security :: Cryptography",
+    ],
     # for cffi
-    cffi_modules=["cpp_code/build_matcher.py:ffibuilder"],
+    cffi_modules=["_cffi_build/build_matcher.py:ffibuilder"],
     zip_safe=False,
-    ext_package="anonlink"
 )
