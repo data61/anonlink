4acd62f02bb57c4420c723a125adce1619ec9d65
Hamish Ivey-Law <hamish.ivey-law@data61.csiro.au>
2018-02-19 16:54:45 +1100

    Tests for arbitrary length popcounts.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3.4
 
 import unittest
+from itertools import combinations_with_replacement
 from anonlink import util
+from bitarray import bitarray
 
 class TestUtilDataGeneration(unittest.TestCase):
 
@@ -19,15 +21,30 @@ class TestUtilDataGeneration(unittest.TestCase):
             self.assertEqual(len(clk[0]), 1024)
             self.assertEqual(clk[0].count(), clk[2])
 
-    def test_popcount_vector(self):
-        bas = [util.generate_bitarray(1024) for i in range(100)]
 
-        popcounts, _ = util.popcount_vector(bas, use_python=True)
-        self.assertEquals(len(popcounts), 100)
-        for i, cnt in enumerate(popcounts):
-            self.assertEquals(cnt, bas[i].count())
+def concat_bitarrays(products):
+    for p in products:
+        yield sum(p, bitarray())
 
-        popcounts, _ = util.popcount_vector(bas, use_python=False)
-        self.assertEquals(len(popcounts), 100)
-        for i, cnt in enumerate(popcounts):
-            self.assertEquals(cnt, bas[i].count())
+# Generate bit arrays that are combinations of words 0, 1, 2^63, 2^64 - 1
+# of various lengths between 1 and 65 words.
+def test_generator():
+    key_lengths = [1, 2, 3, 4, 8, 9, 10, 15, 16, 17,
+                   23, 24, 25, 30, 31, 32, 33, 63, 64, 65]
+    special_words = [64*bitarray('0'),
+                     63*bitarray('0') + bitarray('1'),
+                     bitarray('1') + 63*bitarray('0'),
+                     64*bitarray('1')]
+    for L in key_lengths:
+        words = combinations_with_replacement(special_words, L)
+        # '+' on bitarrays is concatenation
+        bas = [sum(w, bitarray()) for w in words]
+        yield check_popcount_vector, bas
+
+def check_popcount_vector(bas):
+    bas_counts = [b.count() for b in bas]
+
+    popcounts, _ = util.popcount_vector(bas, use_python=True)
+    assert(popcounts == bas_counts)
+    popcounts, _ = util.popcount_vector(bas, use_python=False)
+    assert(popcounts == bas_counts)
