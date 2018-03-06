7c29911e84aa76a489631111d7077a374704f9c6
Guillaume Smith <guillaume.smith@data61.csiro.au>
2018-03-06 11:33:00 +1100

    Correct indent and small things.

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,105 +16,107 @@ def configs = [
     [label: 'osx', pythons: ['python3.5'], compilers: ['clang', 'gcc']]
 ]
 
-def PythonVirtualEnvironment prepareVirtualEnvironment(String pythonVersion, clkhashPackageName, venv_directory=VENV_DIRECTORY) {
-    PythonVirtualEnvironment venv = new PythonVirtualEnvironment(this, venv_directory, pythonVersion);
-    venv.create();
-    venv.runPipCommand("install --upgrade pip coverage setuptools wheel")
-    venv.runPipCommand("install --quiet --upgrade ${clkhashPackageName}");
-    venv.runPipCommand("install -r requirements.txt");
-    venv.runCommand("setup.py sdist bdist_wheel --universal");
-    venv.runPipCommand("install -e .");
-    return venv;
+def PythonVirtualEnvironment prepareVirtualEnvironment(String pythonVersion, clkhashPackageName, venv_directory = VENV_DIRECTORY) {
+  PythonVirtualEnvironment venv = new PythonVirtualEnvironment(this, venv_directory, pythonVersion);
+  venv.create();
+  venv.runPipCommand("install --upgrade pip coverage setuptools wheel")
+  venv.runPipCommand("install --quiet --upgrade ${clkhashPackageName}");
+  venv.runPipCommand("install -r requirements.txt");
+  venv.runCommand("setup.py sdist bdist_wheel --universal");
+  venv.runPipCommand("install -e .");
+  return venv;
 }
 
-def build(python_version, compiler, label, release=false) {
-    GitUtils.checkoutFromSCM(this)
+def build(python_version, compiler, label, release = false) {
+  GitUtils.checkoutFromSCM(this)
+  try {
+    clkhashPackageName = "clkhash-*-py2.py3-none-any.whl"
+    step([$class     : 'CopyArtifact',
+          projectName: 'clkhash/master',
+          fingerprint: true,
+          flatten    : true,
+          filter     : 'dist/' + clkhashPackageName
+    ]);
+
+    PythonVirtualEnvironment venv = prepareVirtualEnvironment(python_version, clkhashPackageName)
     try {
-        clkhashPackageName = "clkhash-*-py2.py3-none-any.whl"
-        step ([$class: 'CopyArtifact',
-               projectName: 'clkhash/master',
-               fingerprint: true,
-               flatten: true,
-               filter: 'dist/' + clkhashPackageName
-        ]);
-
-        PythonVirtualEnvironment venv = prepareVirtualEnvironment(python_version, clkhashPackageName)
-        try {
-            venv.runChosenCommand("nosetests --with-xunit --with-coverage --cover-inclusive --cover-package=anonlink")
-            if(release) {
-                // This will be the official release
-                archiveArtifacts artifacts: "dist/anonlink-*.whl"
-                archiveArtifacts artifacts: "dist/anonlink-*.tar.gz"
-            }
-        } catch (Exception e) {
-            testsError = err
-            currentBuild.result = 'FAILURE'
-            setBuildStatus("Build failed", "FAILURE");
-        } finally {
-            if (!release) {
-                junit 'nosetests.xml'
-            } else {
-                venv.runChosenCommand("coverage xml --omit=\"*/cpp_code/*\" --omit=\"*build_matcher.py*\"")
-                cobertura coberturaReportFile: 'coverage.xml'
-            }
-            if (testsError) {
-                throw testsError
-            }
-        }
-
+      venv.runChosenCommand("nosetests --with-xunit --with-coverage --cover-inclusive --cover-package=anonlink")
+      if (release) {
+        // This will be the official release
+        archiveArtifacts artifacts: "dist/anonlink-*.whl"
+        archiveArtifacts artifacts: "dist/anonlink-*.tar.gz"
+      }
+    } catch (Exception err) {
+      testsError = err
+      currentBuild.result = 'FAILURE'
+      setBuildStatus("Build failed", "FAILURE");
     } finally {
-        deleteDir()
+      if (!release) {
+        junit 'nosetests.xml'
+      } else {
+        venv.runChosenCommand("coverage xml --omit=\"*/cpp_code/*\" --omit=\"*build_matcher.py*\"")
+        cobertura coberturaReportFile: 'coverage.xml'
+      }
+      if (testsError) {
+        throw testsError
+      }
     }
+
+  } finally {
+    echo "Cleanning the workspace"
+    deleteDir()
+    echo "Workspace clean"
+  }
 }
 
 
 def builders = [:]
 for (config in configs) {
-    def label = config["label"]
-    def pythons = config["pythons"]
-    def compilers = config["compilers"]
+  def label = config["label"]
+  def pythons = config["pythons"]
+  def compilers = config["compilers"]
 
-    for (_py_version in pythons) {
-        for (_compiler in compilers) {
+  for (_py_version in pythons) {
+    for (_compiler in compilers) {
 
-            def py_version = _py_version
-            def compiler = _compiler
-            def combinedName = "${label}-${py_version}-${compiler}"
+      def py_version = _py_version
+      def compiler = _compiler
+      def combinedName = "${label}-${py_version}-${compiler}"
 
-            builders[combinedName] = {
-                node(label) {
-                    stage(combinedName) {
-                        build(py_version, compiler, label, false)
-                    }
-                }
-            }
+      builders[combinedName] = {
+        node(label) {
+          stage(combinedName) {
+            build(py_version, compiler, label, false)
+          }
         }
+      }
     }
+  }
 }
 
 GitCommit commit;
-node {
-    commit = GitUtils.checkoutFromSCM(this);
-    commit.setInProgressStatus(GIT_CONTEXT);
+node() {
+  commit = GitUtils.checkoutFromSCM(this);
+  commit.setInProgressStatus(GIT_CONTEXT);
 }
 
 try {
-    parallel builders
-} catch (Exception e){
-    node() {
-        commit.setFailStatus("Build failed", GIT_CONTEXT);
-    }
-    throw err
+  parallel builders
+} catch (Exception e) {
+  node() {
+    commit.setFailStatus("Build failed", GIT_CONTEXT);
+  }
+  throw err
 }
 
 node('GPU 1') {
-    stage('Release') {
-        try {
-            build('python3.5', 'gcc', 'GPU 1', true)
-            commit.setSuccessStatus(GIT_CONTEXT)
-        } catch (Exception e) {
-            commit.setFailStatus("Release failed", GIT_CONTEXT);
-            throw e;
-        }
+  stage('Release') {
+    try {
+      build('python3.5', 'gcc', 'GPU 1', true)
+      commit.setSuccessStatus(GIT_CONTEXT)
+    } catch (Exception e) {
+      commit.setFailStatus("Release failed", GIT_CONTEXT);
+      throw e;
     }
+  }
 }
