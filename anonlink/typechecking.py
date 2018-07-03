6bbb4bd568f03f2a3f225be1aff543f82487f1b5
Jakub Nabaglo <jakub.nabaglo@data61.csiro.au>
2018-07-03 11:30:27 +1000

    Fix typing

--- anonlink/typechecking.py
+++ anonlink/typechecking.py
@@ -1,16 +1,21 @@
 from array import array
 
 from typing import (Callable, Hashable, Iterable, Mapping,
-                    Optional, Sequence, Set, Tuple)
+                    Optional, Sequence, Set, Tuple, TYPE_CHECKING)
 
 
 Record = Sequence[bool]
 
+if TYPE_CHECKING:
+    FloatArrayType = array[float]
+    IntArrayType = array[int]
+else:
+    FloatArrayType = array
+    IntArrayType = array
 
-
-CandidatePairs = Tuple[array,
-                       Tuple[array, ...],
-                       Tuple[array, ...]]
+CandidatePairs = Tuple[FloatArrayType,
+                       Tuple[IntArrayType, ...],
+                       Tuple[IntArrayType, ...]]
 
 BlockingFunction = Callable[[int, int, Record],
                             Iterable[Hashable]]
