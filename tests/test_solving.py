402d62f775b1d09fd63c0daa9a95e8e359f2deb5
Jakub Nabaglo <jakub.nabaglo@data61.csiro.au>
2018-07-10 11:47:47 +1000

    Ok, solving tests should finally work now...

--- tests/test_solving.py
+++ tests/test_solving.py
@@ -1,81 +1,79 @@
+from array import array
+
 from anonlink.solving import greedy_solve
 
 def _zip_candidates(candidates):
     candidates = tuple(candidates)
-    if not candidates:
-        return ((), ()), ((), ()), ()
-    records1, records2, sims = zip(*candidates)
-    dataset_indices = []
-    record_indices = []
-    for (d1, r1), (d2, r2) in zip(records1, records2):
-        dataset_indices.append((d1, d2))
-        record_indices.append((r1, r2))
-    return (tuple(zip(*dataset_indices)),
-            tuple(zip(*record_indices)),
-            sims)
+    sims = array('d')
+    dset_is0 = array('I')
+    dset_is1 = array('I')
+    rec_is0 = array('I')
+    rec_is1 = array('I')
+    for sim, (dset_i0, rec_i0), (dset_i1, rec_i1) in candidates:
+        sims.append(sim)
+        dset_is0.append(dset_i0)
+        dset_is1.append(dset_i1)
+        rec_is0.append(rec_i0)
+        rec_is1.append(rec_i1)
+    return sims, (dset_is0, dset_is1), (rec_is0, rec_is1)
 
 
 def _compare_matching(result, truth):
-    result = set({id(r): frozenset(r) for r in result}.values())
+    result = set(map(frozenset, result))
     truth = set(map(frozenset, truth))
     assert result == truth
 
 
 def test_greedy_twoparty():
-    candidates = [((0, 0), (1, 0), .8)]
+    candidates = [(.8, (0, 0), (1, 0))]
     result = greedy_solve(_zip_candidates(candidates))
     _compare_matching(result, [{(0,0), (1,0)}])
 
-    candidates = [((0, 0), (1, 0), .8),
-                  ((0, 1), (1, 0), .7)]
+    candidates = [(.8, (0, 0), (1, 0)),
+                  (.7, (0, 1), (1, 0))]
     result = greedy_solve(_zip_candidates(candidates))
-    _compare_matching(result, [{(0,0), (1,0)}])
-    result = greedy_solve(_zip_candidates(reversed(candidates)))
     _compare_matching(result, [{(0,0), (1,0)}])
     
     candidates = []
     result = greedy_solve(_zip_candidates(candidates))
     _compare_matching(result, [])
     
-    candidates = [((0, 0), (1, 0), .8),
-                  ((0, 0), (1, 1), .7),
-                  ((0, 1), (1, 0), .7),
-                  ((0, 1), (1, 1), .6)]
+    candidates = [(.8, (0, 0), (1, 0)),
+                  (.7, (0, 0), (1, 1)),
+                  (.7, (0, 1), (1, 0)),
+                  (.6, (0, 1), (1, 1))]
     result = greedy_solve(_zip_candidates(candidates))
-    _compare_matching(result, [{(0,0), (1,0)},
-                               {(0,1), (1,1)}])
-    result = greedy_solve(_zip_candidates(reversed(candidates)))
     _compare_matching(result, [{(0,0), (1,0)},
                                {(0,1), (1,1)}])
 
 
 def test_greedy_threeparty():
-    candidates = [((0, 0), (1, 0), .7),
-                  ((0, 0), (2, 0), .7),
-                  ((1, 0), (2, 0), .9),
-                  ((0, 0), (1, 1), .8),
-                  ((0, 0), (2, 1), .8),
-                  ((1, 1), (2, 1), .8)]
+    candidates = [(.9, (1, 0), (2, 0)),
+                  (.8, (0, 0), (1, 1)),
+                  (.8, (0, 0), (2, 1)),
+                  (.8, (1, 1), (2, 1)),
+                  (.7, (0, 0), (1, 0)),
+                  (.7, (0, 0), (2, 0))]
     result = greedy_solve(_zip_candidates(candidates))
     _compare_matching(result, [{(0,0), (1,1), (2,1)},
                                {(1,0), (2,0)}])
     
-    candidates = [((0, 0), (1, 0), .8),
-                  ((0, 0), (2, 0), .7),
-                  ((0, 1), (1, 1), .7),
-                  ((0, 1), (2, 1), .8),
-                  ((1, 1), (2, 1), .8)]
+    candidates = [(.8, (0, 0), (1, 0)),
+                  (.8, (0, 1), (2, 1)),
+                  (.8, (1, 1), (2, 1)),
+                  (.7, (0, 0), (2, 0)),
+                  (.7, (0, 1), (1, 1))]
     result = greedy_solve(_zip_candidates(candidates))
     _compare_matching(result, [{(0,0), (1,0)},
                                {(0,1), (1,1), (2,1)}])
 
 
 def test_greedy_fourparty():
-    candidates = [((0, 0), (1, 0), .9),
-                  ((2, 0), (3, 0), .9),
-                  ((0, 0), (2, 0), .7),
-                  ((1, 0), (3, 0), .7),
-                  ((0, 0), (3, 0), .7),
-                  ((1, 0), (2, 0), .7)]
+    candidates = [(.9, (0, 0), (1, 0)),
+                  (.9, (2, 0), (3, 0)),
+                  (.7, (0, 0), (2, 0)),
+                  (.7, (1, 0), (3, 0)),
+                  (.7, (0, 0), (3, 0)),
+                  (.7, (1, 0), (2, 0))]
     result = greedy_solve(_zip_candidates(candidates))
     _compare_matching(result, [{(0,0), (1,0), (2,0), (3,0)}])
