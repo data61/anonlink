37efdb5e9760cedfb227f2318f89edd5f275600b
Jakub Nabaglo <j@nab.gl>
2018-07-24 10:23:58 +1000

    Fix inconsistency when threshold is 0 and one of the popcounts is zero (#128)
    * Fix tiebreaking. Fixes #102. I think.
    
    * Fix unexpected results in obscure edge case.
    
    * oops, forgot to early exit.
    
    * Hamish PR comments

--- _cffi_build/dice_one_against_many.cpp
+++ _cffi_build/dice_one_against_many.cpp
@@ -465,8 +465,18 @@ extern "C"
         node_queue top_k_scores(score_cmp(), std::move(vec));
 
         uint32_t count_one = _popcount_array(comp1, keywords);
-        if (count_one == 0)
-            return 0;
+        if (count_one == 0) {
+            if (threshold > 0) {
+                return 0;
+            }
+
+            for (uint32_t j = 0; j < k; ++j) {
+                scores[j] = 0.0;
+                indices[j] = j;
+            }
+
+            return static_cast<int>(k);
+        }
 
         uint32_t max_popcnt_delta = keybytes * CHAR_BIT; // = bits per key
         if(threshold > 0) {
