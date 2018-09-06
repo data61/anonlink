63f659a674090266775796fa6b11d06897221127
Brian Thorne <brian@thorne.link>
2018-09-06 10:29:34 +1000

    Just run auditwheel on anonlink

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -11,12 +11,12 @@ for PYBIN in /opt/python/cp37-cp37m/bin; do
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/anonlink-*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/cp37-cp37m/bin/; do
     "${PYBIN}/pip" install anonlink --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/pytest" anonlink)
+    (cd "$HOME"; "${PYBIN}/pytest" /io/tests)
 done
