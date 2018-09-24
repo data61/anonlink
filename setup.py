55d7fb0552a5904948a7238b1dfa1efe2b0a019d
Jakub Nabaglo <j@nab.gl>
2018-09-24 12:32:42 +1000

    Release 0.10.0 (#164)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name="anonlink",
-    version='0.9.1-dev',
+    version='0.10.0',
     description='Anonymous linkage using cryptographic hashes and bloom filters',
     long_description=readme,
     long_description_content_type='text/x-rst',
