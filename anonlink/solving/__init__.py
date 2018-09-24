fda15e6d6f8b8b295ce0e38650136508179f4ba5
Jakub Nabaglo <jakub.nabaglo@data61.csiro.au>
2018-09-24 11:54:33 +1000

    Minor: docstring

--- anonlink/solving/__init__.py
+++ anonlink/solving/__init__.py
@@ -1,4 +1,5 @@
 """Solvers.
+
 Solvers accept candidate pairs and return a concrete matching. In other
 words, they accept a similarity for every candidate pair and turn it
 into a boolean for every candidate pair.
