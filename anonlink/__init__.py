6184475138a2750ad0750aecd66367dfd5327941
Jakub Nabaglo <j@nab.gl>
2018-10-24 15:59:32 +1100

    Import similarities in __init__ (#172)
    * Import similarities in __init__

--- anonlink/__init__.py
+++ anonlink/__init__.py
@@ -7,6 +7,7 @@ from anonlink import concurrency
 from anonlink import entitymatch
 from anonlink import network_flow
 from anonlink import serialization
+from anonlink import similarities
 from anonlink import solving
 from anonlink import typechecking
 
