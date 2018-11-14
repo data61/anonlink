8883ec42706bbfaf7ce55bd3883e6375b549e8a3
Jakub Nabaglo <j@nab.gl>
2018-11-14 15:33:14 +1100

    Add probabilistic greedy solver (#179)
    * Add probabilistic greedy solver
    
    * Hamish PR comments

--- anonlink/solving/_multiparty_solving_inner.cpp
+++ anonlink/solving/_multiparty_solving_inner.cpp
@@ -49,6 +49,7 @@ public:
         } else {
             Group *retval = res_iterator->second;
             assert(retval);
+            assert(retval->size());
             return retval;
         }
     }
@@ -82,6 +83,7 @@ public:
         assert(!in_group(record));
         assert(group);
         assert(group_consistent(group));
+        assert(group->size());
 
         group->push_back(record);
         record_group_map[record] = group;
@@ -98,6 +100,8 @@ public:
         assert(absorbee != absorber);
         assert(group_consistent(absorber));
         assert(group_consistent(absorbee));
+        assert(absorber->size());
+        assert(absorbee->size());
 
         // Move records over.
         absorber->reserve(absorber->size() + absorbee->size());
@@ -119,6 +123,7 @@ public:
         std::unordered_set<Group *> unique_groups;
         for (const auto &item : record_group_map) {
             Group * const group = item.second;
+            assert(group->size());
             unique_groups.insert(group);
         }
 
@@ -170,7 +175,7 @@ public:
     CountType increment(T key0, T key1) {
         // Call set_or_increment twice for symmetry.
         CountType retval0 = set_or_increment(key0, key1, 1);
-        CountType retval1 = set_or_increment(key1, key0, 1);
+        CountType retval1[[maybe_unused]] = set_or_increment(key1, key0, 1);
         
         assert(retval0 == retval1); // Assert symmetric.
         
@@ -215,18 +220,45 @@ public:
 };
 
 
-void none_grouped(GroupsStore &groups_store, Record i0, Record i1) {
-    // Neither is in a group, so let's make one.
-    groups_store.make_group(i0, i1);
+// Check if there exists a datset that has more than one element in the two groups.
+// If so, and if we assume that the two datasets are deduplicated, then we cannot merge the groups.
+bool check_no_duplicates(Record i0, Record i1) {
+    return i0.dset_i != i1.dset_i;
+}
+bool check_no_duplicates(Record i0, Group *group1) {
+    return std::all_of(group1->cbegin(), group1->cend(),
+                       [i0](Record r) {
+                           return check_no_duplicates(i0, r);
+                       });
+}
+bool check_no_duplicates(Group *group0, Group *group1) {
+    return std::all_of(group0->cbegin(), group0->cend(),
+                       [group1](Record r) {
+                           return check_no_duplicates(r, group1);
+                       });
+}
+
+
+void none_grouped(GroupsStore &groups_store, Record i0, Record i1, bool deduplicated) {
+    if (!deduplicated || check_no_duplicates(i0, i1)) {
+        // Neither is in a group, so let's make one.
+        groups_store.make_group(i0, i1);
+    }
+    // If they are duplicates from the same datasets, then they will never be in the same group, so
+    // there is no point making singleton groups for them.
 }
 
 void one_grouped(GroupsStore &groups_store,
                  EdgesMatrix<Group *> &edges_store,
                  Group *group,
-                 Record i) {
-    if (group->size() == 1) {
-        // We have two singletons (one has a group of itself, one doesn't), so we can merge them.
-        groups_store.add_to_group(group, i);
+                 Record i,
+                 double merge_threshold,
+                 bool deduplicated) {
+    if (1.0 >= merge_threshold * group->size()) {
+        if (!deduplicated || check_no_duplicates(i, group)) {
+            // We have two singletons (one has a group of itself, one doesn't), so we can merge.
+            groups_store.add_to_group(group, i);
+        }
     } else {
         // The group has at least 2 elements. We've only matched with one so far (or else we'd
         // already have a group of size at least one), so we can't merge.
@@ -249,22 +281,26 @@ void two_grouped_merge(GroupsStore &groups_store,
 void two_grouped(GroupsStore &groups_store,
                  EdgesMatrix<Group *> &edges_store,
                  Group *group0,
-                 Group *group1) {
+                 Group *group1,
+                 double merge_threshold,
+                 bool deduplicated) {
     if (group0 == group1) {
         return; // Already grouped together. Nothing to do.
     }
 
-    auto overlap = edges_store.increment(group0, group1);
+    double overlap = edges_store.increment(group0, group1);
     auto group_0_size = group0->size();
     auto group_1_size = group1->size();
     // The below is equivalent to: for every pair of records in the Cartesian product of group 0 and
     // group 1, we've encountered an edge.
-    if (overlap == group_0_size * group_1_size) {
-        // Optimise by enlarging the bigger group.
-        if (group_0_size < group_1_size) {
-            two_grouped_merge(groups_store, edges_store, group1, group0);
-        } else {
-            two_grouped_merge(groups_store, edges_store, group0, group1);
+    if (overlap >= merge_threshold * group_0_size * group_1_size) {
+        if (!deduplicated || check_no_duplicates(group0, group1)) {
+            // Optimise by enlarging the bigger group.
+            if (group_0_size < group_1_size) {
+                two_grouped_merge(groups_store, edges_store, group1, group0);
+            } else {
+                two_grouped_merge(groups_store, edges_store, group0, group1);
+            }
         }
     }
 }
@@ -272,13 +308,13 @@ void two_grouped(GroupsStore &groups_store,
 } /* namespace */
 
 std::unordered_set<Group *>
-greedy_solve_inner(
-                   unsigned int dset_is0[],
+greedy_solve_inner(unsigned int dset_is0[],
                    unsigned int dset_is1[],
                    unsigned int rec_is0[],
                    unsigned int rec_is1[],
-                   size_t n
-                   ) {
+                   size_t n,
+                   double merge_threshold,
+                   bool deduplicated) {
     // Keep track of groups that have already been formed.
     GroupsStore groups_store;
 
@@ -299,16 +335,25 @@ greedy_solve_inner(
         Group *group_i1 = groups_store.get_group(i1);
         
         if (group_i0) {
+            assert(group_i0->size());
             if (group_i1) {
-                two_grouped(groups_store, edges_store, group_i0, group_i1);
+                assert(group_i1->size());
+                two_grouped(groups_store, edges_store,
+                            group_i0, group_i1,
+                            merge_threshold, deduplicated);
             } else {
-                one_grouped(groups_store, edges_store, group_i0, i1);
+                one_grouped(groups_store, edges_store,
+                            group_i0, i1,
+                            merge_threshold, deduplicated);
             }
         } else {
             if (group_i1) {
-                one_grouped(groups_store, edges_store, group_i1, i0);
+                assert(group_i1->size());
+                one_grouped(groups_store, edges_store,
+                            group_i1, i0,
+                            merge_threshold, deduplicated);
             } else {
-                none_grouped(groups_store, i0, i1);
+                none_grouped(groups_store, i0, i1, deduplicated);
             }
         }
     }
--- anonlink/solving/_multiparty_solving_python.py
+++ anonlink/solving/_multiparty_solving_python.py
@@ -5,30 +5,67 @@ import typing as _typing
 import anonlink.typechecking as _typechecking
 
 
-def greedy_solve_python(
-    candidates: _typechecking.CandidatePairs
+def probabilistic_greedy_solve_python(
+    candidates: _typechecking.CandidatePairs,
+    *,
+    merge_threshold: float = .5,
+    deduplicated: bool = True
 ) -> _typechecking.MatchGroups:
-    """ Select matches from candidate pairs using the greedy algorithm.
+    """Select matches using the probabilistic greedy algorithm.
+
+    This solver is similar to `greedy_solve_native`, but may match
+    records that do not have corresponding candidate pairs when there is
+    enough other evidence that they are a match. This usually results in
+    better accuracy.
 
     We assign each record to exactly one 'group' of records that are
     pairwise matched together. We iterate over `candidates` in order of
     decreasing similarity. When we encounter a pair of records (let s be
     their similarity) that do not already belong to the same group, we
-    merge their groups iff (a) every pair of records is permitted to be
-    matched, and (b) the similarity of every pair of records is above s.
-    The latter requirement is only for tie-breaking: we prefer groups
-    whose minimum pairwise similarity is highest. Any group merge
-    rejected due to requirement (b) will be revisited later with that
-    requirement relaxed.
+    merge their groups iff (a) the proportion of pairs that are
+    permitted to be matched in the groups' records is at least
+    `merge_threshold`, and (b) the proportion of pairs whose similarity
+    is above s in the groups' records is at least `merge_threshold`. The
+    latter requirement is only for tie-breaking: we prefer groups whose
+    minimum pairwise similarity is highest. Any group merge rejected due
+    to requirement (b) will be revisited later with that requirement
+    relaxed.
 
     :param tuple candidates: Candidates, as returned by
         `find_candidates`.
+    :param float merge_threshold: Proportion of agreement between groups
+        required to merge them. In (0, 1].
+            When considering whether to merge two groups, we compute the
+        groups' Cartesian product. We count the pairs in that product
+        whose similarity is above the threshold. We merge the groups if
+        our count is at least `merge_threshold` of the total number of
+        pairs between the two groups.
+            For example, suppose that we have two groups of records: 
+        {A, B} and {C, D}. Let A-C, A-D and B-C be candidate pairs.
+        Setting `merge_threshold` to any value not greater than .75 will
+        permit the two groups to be merged since they share three
+        candidate pairs out of all four pairs. Even though the
+        similarity of B-D is below the threshold, there is enough
+        evidence in the other pairs to conclude that it must be a match.
+        The `merge_threshold` can be thought of as the inverse of the
+        weight we place on that evidence.
+            Higher values increase precision, but decrease recall.
+            A `merge_threshold` lower than one permits two records to be
+        matched without having a corresponding candidate pair. Set it to
+        1.0 to ensure that two records will only be matched if their
+        similarity is above the similarity threshold.
+    :param bool deduplicated: When True, two records that belong to the
+        same dataset will never be in the same group. Default True.
 
-    :return: An iterable of groups. Each group is an iterable of
+    :return: An sequence of groups. Each group is an sequence of
         records. Two records are in the same group iff they represent
         the same entity. Here, a record is a two-tuple of dataset index
         and record index.
     """
+    if merge_threshold < 0 or merge_threshold > 1:
+        raise ValueError(
+            f'merge_threshold must be between 0 and 1 (got {merge_threshold})') 
+
     sims, dset_is, rec_is = candidates
     if len(dset_is) != len(rec_is):
         raise ValueError('inconsistent shape of index arrays')
@@ -79,8 +116,12 @@ def greedy_solve_python(
             # of pairs is len(i0_matches) * len(i1_matches)). When this
             # is the number of matchable pairs, then every pair is
             # matchable.
-            if (matchable_pairs[i0_mid][i1_mid] + 1
-                == len(i0_matches) * len(i1_matches)):
+            overlap = matchable_pairs[i0_mid][i1_mid] + 1
+            total_pairs = len(i0_matches) * len(i1_matches)
+            duplicates_ok = (not deduplicated or all(m0 != m1
+                                                     for m0, _ in i0_matches
+                                                     for m1, _ in i1_matches))
+            if overlap >= merge_threshold * total_pairs and duplicates_ok:
                 # Optimise by always extending the bigger group.
                 if len(i0_matches) < len(i1_matches):
                     i0, i1 = i1, i0
@@ -114,7 +155,11 @@ def greedy_solve_python(
             # i0 is in a group, but i1 is not.
             # See if we may assign i0 to that group.
             i0_matches = matches[i0]
-            if len(i0_matches) == 1:
+            overlap = 1
+            total_pairs = len(i0_matches)
+            duplicates_ok = not deduplicated or all(m0 != i1[0]
+                                                    for m0, _ in i0_matches)
+            if overlap >= merge_threshold * total_pairs and duplicates_ok:
                 # i0 is a group of 1, so trivially we can merge.
                 i0_matches.append(i1)
                 matches[i1] = i0_matches
@@ -130,8 +175,10 @@ def greedy_solve_python(
             continue
 
         if i0 not in matches and i1 not in matches:
-            # Neither is in a group, so let's just make one.
-            matches[i0] = matches[i1] = [i0, i1]
+            duplicates_ok = not deduplicated or i0[0] != i1[0]
+            if duplicates_ok:
+                # Neither is in a group, so let's just make one.
+                matches[i0] = matches[i1] = [i0, i1]
             continue
 
         raise RuntimeError('non-exhaustive cases')
@@ -141,3 +188,31 @@ def greedy_solve_python(
                            for group in matches.values()
                            if len(group) > 1}
     return tuple(map(tuple, deduplicated_groups.values()))
+
+
+def greedy_solve_python(
+    candidates: _typechecking.CandidatePairs
+) -> _typechecking.MatchGroups:
+    """Select matches from candidate pairs using the greedy algorithm.
+
+    We assign each record to exactly one 'group' of records that are
+    pairwise matched together. We iterate over `candidates` in order of
+    decreasing similarity. When we encounter a pair of records (let s be
+    their similarity) that do not already belong to the same group, we
+    merge their groups iff (a) every pair of records is permitted to be
+    matched, and (b) the similarity of every pair of records is above s.
+    The latter requirement is only for tie-breaking: we prefer groups
+    whose minimum pairwise similarity is highest. Any group merge
+    rejected due to requirement (b) will be revisited later with that
+    requirement relaxed.
+
+    :param tuple candidates: Candidates, as returned by
+        `find_candidates`.
+
+    :return: An sequence of groups. Each group is an sequence of
+        records. Two records are in the same group iff they represent
+        the same entity. Here, a record is a two-tuple of dataset index
+        and record index.
+    """
+    return probabilistic_greedy_solve_python(
+        candidates, merge_threshold=1.0, deduplicated=False)
