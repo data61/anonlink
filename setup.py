dbce90d65dd61b27e809cedcb8da067c65797901
Brian Thorne <brian.thorne@data61.csiro.au>
2019-06-27 10:22:20 +1000

    Release v0.12.2 (#215)
    * Travis can also deploy the sdist to pypi
    * Only upload manylinux wheel
    * Update versions of pinned requirements
    * Update changelog and version for 0.12.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name="anonlink",
-    version='0.12.1',
+    version='0.12.2',
     description='Anonymous linkage using cryptographic hashes and bloom filters',
     long_description=readme,
     long_description_content_type='text/x-rst',
--- travis/build-dist.sh
+++ travis/build-dist.sh
@@ -9,6 +9,8 @@ export PYBIN="/opt/python/cp37-cp37m/bin"
 
 "${PYBIN}/pip" install -r /io/requirements.txt
 "${PYBIN}/pip" install -e /io/
+cd /io
+"${PYBIN}/python" setup.py sdist -d wheelhouse
 "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
