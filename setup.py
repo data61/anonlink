6eeca3c73ab32ada8c648109d7576d47d0229c68
Brian Thorne <brian@thorne.link>
2019-06-28 10:30:13 +1000

    Set version to 0.12.3
    Remove beta version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name="anonlink",
-    version='0.12.3b1',
+    version='0.12.3',
     description='Anonymous linkage using cryptographic hashes and bloom filters',
     long_description=readme,
     long_description_content_type='text/x-rst',
