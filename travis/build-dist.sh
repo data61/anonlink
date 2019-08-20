f09ee77fc799017112766acf6b1512f7f0dadcba
Brian Thorne <brian@thorne.link>
2019-08-20 22:14:26 +1000

    Limit to just Python3.5 and above

--- travis/build-dist.sh
+++ travis/build-dist.sh
@@ -8,8 +8,14 @@ yum install -y atlas-devel
 
 ls /opt/python/
 
+listOfPythons="
+cp35-cp35m
+cp36-cp36m
+cp37-cp37m"
+
 # Compile wheels for each Python version
-for PYBIN in /opt/python/cp3*/bin; do
+for pyPath in $listOfPythons; do
+  PYBIN="/opt/python/$pyPath/bin"
   "${PYBIN}/pip" install -r /io/requirements.txt
   "${PYBIN}/pip" install -e /io/
   cd /io
@@ -22,8 +28,9 @@ for whl in wheelhouse/anonlink-*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
-# Install packages and test
-for PYBIN in /opt/python/cp3*/bin; do
+# Install packages and test for each Python version
+for pyPath in $listOfPythons; do
+    PYBIN="/opt/python/$pyPath/bin"
     "${PYBIN}/pip" install anonlink --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/pytest" /io/tests -W ignore::DeprecationWarning)
 done
