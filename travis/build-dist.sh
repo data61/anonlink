595cff38b1f8fb1bcf3c4d1b9ca074d7a0450106
Brian Thorne <brian.thorne@data61.csiro.au>
2019-08-21 20:06:52 +1000

    Build and test wheel for each Python version in manylinux1 container (#232)

--- travis/build-dist.sh
+++ travis/build-dist.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
-set -e -x
-# Script is assumed to be running inside a 64bit manylinux container
+# Script is assumed to be running inside a manylinux container
 # See https://github.com/pypa/manylinux
+
+set -e -x
+
 yum install -y atlas-devel
 
-# Compile wheels
-export PYBIN="/opt/python/cp37-cp37m/bin"
+ls /opt/python/
 
-"${PYBIN}/pip" install -r /io/requirements.txt
-"${PYBIN}/pip" install -e /io/
-cd /io
-"${PYBIN}/python" setup.py sdist -d wheelhouse
-"${PYBIN}/pip" wheel /io/ -w wheelhouse/
+listOfPythons="
+cp36-cp36m
+cp37-cp37m"
+
+# Compile wheels for each Python version
+for pyPath in $listOfPythons; do
+  PYBIN="/opt/python/$pyPath/bin"
+  "${PYBIN}/pip" install -r /io/requirements.txt
+  "${PYBIN}/pip" install -e /io/
+  cd /io
+  "${PYBIN}/python" setup.py sdist -d wheelhouse
+  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/anonlink-*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
-# Install packages and test
-"${PYBIN}/pip" install anonlink --no-index -f /io/wheelhouse
-(cd "$HOME"; "${PYBIN}/pytest" /io/tests -W ignore::DeprecationWarning)
+# Install packages and test for each Python version
+for pyPath in $listOfPythons; do
+    PYBIN="/opt/python/$pyPath/bin"
+    "${PYBIN}/pip" install anonlink --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/pytest" /io/tests -W ignore::DeprecationWarning)
+done
