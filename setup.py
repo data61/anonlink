66c39c5d0413019f66d24fee56242cb45c8b5e43
Brian Thorne <brian.thorne@data61.csiro.au>
2019-08-22 11:08:12 +1000

    Build wheels for multiple platforms (#234)
    * Build and test wheel on manylinux2010 as well as manylinux1
    
    Set version to 0.12.5 alpha4

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name="anonlink",
-    version='0.12.5a1',
+    version='0.12.5a4',
     description='Anonymous linkage using cryptographic hashes and bloom filters',
     long_description=readme,
     long_description_content_type='text/x-rst',
